ed5035819aabb28cecefd3736221cfa69a9ae980
root <root@localhost.localdomain>
2018-01-25 11:28:18 +0800

    test boss

--- test1.py
+++ test1.py
@@ -1,3 +1,7 @@
 hello
 hello2
 hello3
+Git is a distributed version control system.
+Git is free software distributed under the GPL.
+Git has a mutable index called stage.
+Git tracks changes of files.
