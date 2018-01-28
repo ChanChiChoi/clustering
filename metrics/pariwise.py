f81ab911b289c1475209f4e2eaf493cda90312b3
unknown <342888106@qq.com>
2018-01-28 21:34:04 +0800

    extract the check into utils

--- metrics/pariwise.py
+++ metrics/pariwise.py
@@ -6,41 +6,7 @@ from scipy.sparse import issparse
 import numba 
 #import pp #parallel python
 
-# Utility Functions
-# the function borrow from 
-#https://github.com/scikit-learn/scikit-learn/blob/master/sklearn/metrics/pairwise.py
-def _return_float_dtype(X, Y):
-    """
-    1. If dtype of X and Y is float32, then dtype float32 is returned.
-    2. Else dtype float is returned.
-    """
-    if not issparse(X) and not isinstance(X, np.ndarray):
-        X = np.asarray(X)
-
-    if Y is None:
-        Y_dtype = X.dtype
-    elif not issparse(Y) and not isinstance(Y, np.ndarray):
-        Y = np.asarray(Y)
-        Y_dtype = Y.dtype
-    else:
-        Y_dtype = Y.dtype
-
-    if X.dtype == Y_dtype == np.float32:
-        dtype = np.float32
-    else:
-        dtype = np.float
-
-    return X, Y, dtype
-
-def _check_pairwise_arrays(X,Y):
-
-    X, Y, dtype_float = _return_float_dtype(X, Y)
-    x_shape = X.shape
-    y_shape = Y.shape   
-    assert x_shape == y_shape, 'X shape not equal Y shape'
-    if X.ndim == 1: X = X.reshape(1,-1)
-    if Y.ndim == 1: Y = Y.reshape(1,-1)
-    return X,Y 
+from .utils import check_pairwise_arrays
 
 def euclidean_distances(X,Y):
     """
@@ -65,7 +31,7 @@ def euclidean_distances(X,Y):
     >>> timeit ed(X,X)
     10 loops, best of 3: 161 ms per loop
     """ 
-    X,Y = _check_pairwise_arrays(X,Y)
+    X,Y = check_pairwise_arrays(X,Y)
 
     X_square = X*X
     XX = X_square.sum(1).reshape(-1,1)
